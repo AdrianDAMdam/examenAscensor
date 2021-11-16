ccc80bacd79c1f56dff83ae5cd2f4137dbf3de25
AdrianDAMdam <adrian.torres16@iesdoctorbalmis.com>
2021-11-16 23:23:02 +0100

    primer commit

---
+++ ej1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ej1
+{
+    //     Vamos a construir un programa que calcule y muestre el precio de un terreno dependiendo de su tamaño, del tipo de
+    // terreno que se quiera comprar y de un descuento aplicado.
+    // Necesitaremos saber el tipo de terreno (urbano o rústico), controlando que la entrada sea correcta e
+    // independientemente de mayúsculas o minúsculas. También recogeremos la información del largo y ancho del terreno
+    // (float), para calcular los metros cuadrados (largo * ancho).
+    // Con toda la información calcularemos el precio de coste total, teniendo en cuenta que: el terreno rústico cuesta 20 euros el
+    // m2 y el urbano 150 euros el m2. Se aplicará un descuento según las siguientes premisas:
+    // Si el terreno tiene más de 400 metros cuadrados se hace un descuento del 10% si el terreno tiene más de 500 metros
+    // cuadrados el descuento es del 17%.
+    // ✋ Nota: Realizar el ejercicio con switch when. Tendrás que crear los métodos necesarios para conseguir una buena    // programación modular.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tipoTerreno;
+            do
+            {
+                System.Console.Write("Indica tipo de terreno(rustico/urbano): ");
+                tipoTerreno = Console.ReadLine().ToUpper();
+            } while (tipoTerreno != "RUSTICO" && tipoTerreno != "URBANO");
+
+            System.Console.Write("Introduce ancho terreno: ");
+            float ancho = float.Parse(Console.ReadLine());
+            System.Console.Write("Introduce largo terreno: ");
+            float largo = float.Parse(Console.ReadLine());
+
+            string texto = "";
+            double precioFinca;
+            float areaTerreno = ancho * largo;
+            const double PRECIO_TERRENO_URBANO=150;
+            const double PRECIO_TERRENO_RUSTICO=20;
+
+
+            switch (tipoTerreno)
+            {
+                case "URBANO" when (areaTerreno) > 500:
+                    precioFinca = areaTerreno * PRECIO_TERRENO_URBANO * 0.83;
+                    texto = $"El terreno mide mas de 500 m2 y se le hace un descueto del 17%\n{areaTerreno} m2 cuesta {precioFinca}";
+                    break;
+                case "URBANO" when (areaTerreno) > 400:
+                    precioFinca = areaTerreno * PRECIO_TERRENO_URBANO * 0.90;
+                    texto = $"El terreno mide mas de 400 m2 y se le hace un descueto del 10%\n{areaTerreno} m2 cuesta {precioFinca}";
+                    break;
+                case "URBANO":
+                    precioFinca = areaTerreno * PRECIO_TERRENO_URBANO;
+                    break;
+
+               case "RUSTICO" when (areaTerreno) > 500:
+                    precioFinca = areaTerreno * PRECIO_TERRENO_RUSTICO * 0.83;
+                    texto = $"El terreno mide mas de 500 m2 y se le hace un descueto del 17%\n{areaTerreno} m2 cuesta {precioFinca}";
+                    break;
+                case "RUSTICO" when (areaTerreno) > 400:
+                    precioFinca = areaTerreno * PRECIO_TERRENO_RUSTICO * 0.90;
+                    texto = $"El terreno mide mas de 400 m2 y se le hace un descueto del 10%\n{areaTerreno} m2 cuesta {precioFinca}";
+                    break;
+                case "RUSTICO":
+                    precioFinca = areaTerreno * PRECIO_TERRENO_RUSTICO;
+                    break;
+                default:
+                    texto = "";
+                    break;
+            }
+            System.Console.WriteLine(texto);
+
+
+
+        }
+    }
+}
---
+++ ej2/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace ej2
+{
+    class Program
+    {
+        const short NUMEROPLANTAS = 7;
+        const short NUMEROLOCALESPLANTA = 6;
+        const short EMPLEADOSPORLOCAL = 5;
+        const short VISITASMAXIMAS = 10;
+        const short VISITASMINIMAS = 5;
+        const short PORCENTAJEUSOASCENSOR = 2;
+        static short PideAscensor()
+        {
+            short planta;
+            do
+            {
+                System.Console.Write("En que planta estas: ");
+                planta = short.Parse(Console.ReadLine());
+                if (planta > 7 || planta < 0)
+                    System.Console.WriteLine("Introduce planta valida");
+            } while (planta > 7 || planta < 0);
+
+            return planta;
+        }
+        static float PersonasQuePuedeHaberEnElEdificio()
+        {
+            int locales = NUMEROPLANTAS * NUMEROLOCALESPLANTA;
+            int personasEdificio = locales * EMPLEADOSPORLOCAL;
+            Random aleatorio = new Random();
+
+            int personasEdificioHoraPunta = locales * aleatorio.Next(VISITASMINIMAS, VISITASMAXIMAS + 1) + personasEdificio;
+            int personasEdificioHoraNormal = personasEdificio + aleatorio.Next(-personasEdificio * 5 / 100, personasEdificio * 100 / 100);
+            float personasQuePuedenEstarUsandoAscensor = personasEdificio * (PORCENTAJEUSOASCENSOR / 100f);
+            return personasQuePuedenEstarUsandoAscensor;
+        }
+        static bool EstaLibre()
+        {
+            float personasQuePuedenEstarUsandoAscensor = PersonasQuePuedeHaberEnElEdificio();
+            bool esLibre;
+            Random semilla = new Random();
+            if (semilla.Next(0, (int)personasQuePuedenEstarUsandoAscensor) > personasQuePuedenEstarUsandoAscensor / 2)
+                esLibre = true;
+            else
+                esLibre = false;
+
+            return esLibre;
+        }
+        static int PlantaPardo(int planta)
+        {
+            int plantaPardo;
+            do
+            {
+                Random semilla = new Random();
+                plantaPardo = semilla.Next(0, NUMEROPLANTAS + 1);
+            } while (planta == plantaPardo);
+
+            return plantaPardo;
+        }
+        static void SubePlanta(int plantaParado, int planta)
+        {
+            string texto = "";
+            while (planta > plantaParado)
+            {
+                texto = $"Ascensor en planta - {plantaParado} - y subiendo...";
+                plantaParado += 1;
+                System.Console.WriteLine(texto);
+                System.Threading.Thread.Sleep(1500);
+            }
+            HaLLegadoAPlanta(planta);
+        }
+        static void BajaPlanta(int plantaParado, int planta)
+        {
+            string texto = "";
+            while (planta > plantaParado)
+            {
+                texto = $"Ascensor en planta - {plantaParado} - y bajando...";
+                plantaParado += 1;
+                System.Console.WriteLine(texto);
+                System.Threading.Thread.Sleep(1000);
+            }
+            System.Console.WriteLine();
+            HaLLegadoAPlanta(planta);
+
+        }
+        static void HaLLegadoAPlanta(int planta)
+        {
+            string texto = $"El ascensor ya ha llegado a la planta {planta}, puede entrar";
+            System.Console.WriteLine(texto);
+        }
+        static void LlegaAscensor(int plantaParado, int planta)
+        {
+            if (planta > plantaParado)
+            {
+                SubePlanta(plantaParado, planta);
+
+            }
+            else if (planta < plantaParado)
+            {
+                BajaPlanta(plantaParado, planta);
+
+            }
+            else
+            {
+                HaLLegadoAPlanta(planta);
+            }
+
+        }
+        static void EsperaAlAscensor(int planta)
+        {
+            bool esLibre;
+            string texto;
+            do
+            {
+                esLibre = EstaLibre();
+                texto = (esLibre ? "El ascenso esta libre" : "El ascensor no esta libre.");
+                System.Threading.Thread.Sleep(500);
+                System.Console.WriteLine(texto);
+            } while (!esLibre);
+            int plantaParado = PlantaPardo(planta);
+            LlegaAscensor(plantaParado, planta);
+
+
+
+        }
+        static void Main(string[] args)
+        {
+
+            short planta = PideAscensor();
+            EsperaAlAscensor(planta);
+            //pueba cosas git
+            
+
+        }
+
+
+    }
+}
